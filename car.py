f11b510a500eac28f116eecad9f8f695d0dee60e
gbuxss <jeevandhamala095@gmail.com>
2022-02-12 16:57:17 -0600

    added code to check if car is in stop or start condition

---
+++ car.py
@@ -0,0 +1,33 @@
+# program to check multiple options: car game
+
+'''input commands: help
+start- to start the car
+stop - to stop the car
+quit- to exit
+
+except these options display i don't understand message
+'''
+
+command = ""
+started = False
+while True:
+    command = input(">").lower()
+    if command == "help":
+        print("start- to start the car\nstop - to stop the car\nquit- to exit")
+    elif command == "start":
+        if started:
+            print("Car is already started")
+        else:
+            started = True
+            print("Engine Start")
+    elif command == "stop":
+        if not started:
+            print("Car already stopped")
+        else:
+            started = False
+            print("Engine stopped")
+    elif command == "quit":
+        print("Bye !!! \nSee you later")
+        break
+    else:
+        print("Wrong command \nType 'help' to get help or 'quit' to terminate. ")
